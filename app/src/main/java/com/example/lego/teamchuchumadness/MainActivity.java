67e04533e3fc137bf9f87a7515d314999c925b57
Legementario <mr.re66it@gmail.com>
2016-03-21 20:38:39 +0200

    v0.6

--- app/src/main/java/com/example/lego/teamchuchumadness/MainActivity.java
+++ app/src/main/java/com/example/lego/teamchuchumadness/MainActivity.java
@@ -1,55 +1,42 @@
 package com.example.lego.teamchuchumadness;
 
-import android.app.Dialog;
-import android.support.v4.app.DialogFragment;
+
+import android.content.ActivityNotFoundException;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements
-        AdapterView.OnItemSelectedListener {
 
-    private TextView mSelectText;
-    private TeamAdapter mAdapter;
+public class MainActivity extends AppCompatActivity  {
+
+    ViewTeam myDialogFragment;
+    Button teamLogo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.team_logo_dialog);
+        setContentView(R.layout.activity_main);
+        myDialogFragment = new ViewTeam();
+        teamLogo = (Button) findViewById(R.id.bt_choose_team);
 
-//        mSelectText = (TextView) findViewById(R.id.info);
-        final GridView g = (GridView) findViewById(R.id.gridView);
-        mAdapter = new TeamAdapter(getApplicationContext(),
-               R.layout.team_logo_dialog);
-        g.setAdapter(mAdapter);
-        g.setOnItemSelectedListener(this);
-        g.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
+        teamLogo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                // TODO Auto-generated method stub
-//                mSelectText.setText("Выбранный элемент: "
-//                        + mAdapter.getItem(position));
+            public void onClick(View v) {
+                try {
+                    FragmentManager manager = getSupportFragmentManager();
+                    myDialogFragment.show(manager, "dialog");
+                } catch (ActivityNotFoundException e) {
+                    String errorMessage = "ololo";
+                    Toast toast = Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
+
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View v, int position,
-                               long id) {
-//        mSelectText.setText("Выбранный элемент: " + mAdapter.getItem(position));
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-//        mSelectText.setText("Выбранный элемент: ничего");
-    }
 }
---
+++ app/src/main/java/com/example/lego/teamchuchumadness/ViewTeam.java
@@ -0,0 +1,50 @@
+package com.example.lego.teamchuchumadness;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+/**
+ * @author Lego on 21.03.2016.
+ */
+public class ViewTeam extends DialogFragment implements
+        AdapterView.OnItemSelectedListener{
+
+
+    private TeamAdapter mAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.team_logo_dialog, container, false);
+        final GridView g = (GridView) rootView.findViewById(R.id.gridView);
+        mAdapter = new TeamAdapter(inflater.getContext().getApplicationContext(),
+                R.layout.team_logo_dialog);
+        g.setAdapter(mAdapter);
+        g.setOnItemSelectedListener(this);
+        g.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position,
+                               long id) {
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+}
