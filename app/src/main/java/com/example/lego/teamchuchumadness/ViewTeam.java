50bac0ef6545d3bef1a6b8a8759ace264c7bfd1f
lego <mr.re66it@gmail.com>
2016-03-23 12:39:40 +0200

    НЕ БЫЛО! НЕ БЫЛО НЕ ЕДИНОГО РАЗРЫВААА!!

--- app/src/main/java/com/example/lego/teamchuchumadness/ViewTeam.java
+++ app/src/main/java/com/example/lego/teamchuchumadness/ViewTeam.java
@@ -19,6 +19,7 @@ public class ViewTeam extends DialogFragment implements
 
     private TeamAdapter mAdapter;
     private String[] temp;
+    private int step;
 
     Integer[] mImage = new Integer[]{R.drawable.fl0, R.drawable.fl1,
             R.drawable.fl2, R.drawable.fl3, R.drawable.fl4,
@@ -30,6 +31,7 @@ public class ViewTeam extends DialogFragment implements
         final View rootView = inflater.inflate(R.layout.team_logo_dialog, container, false);
         final GridView g = (GridView) rootView.findViewById(R.id.gridView);
         temp = getResources().getStringArray(R.array.country);
+        step = 0;
         mAdapter = new TeamAdapter(inflater.getContext().getApplicationContext(),
                 R.layout.team_logo_dialog, temp, mImage);
         if (g.getAdapter() == null) {
@@ -40,15 +42,61 @@ public class ViewTeam extends DialogFragment implements
 
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                step++;
                 Button teamLogo = (Button) getActivity().findViewById(R.id.bt_choose_team);
                 teamLogo.setText(temp[position]);
-                Drawable img = getContext().getResources().getDrawable(mImage[position]);
-                img.setBounds( 0, 0, 50, 35 );
-                teamLogo.setCompoundDrawables( img, null, null, null );
-                temp = getResources().getStringArray(R.array.football_team_Ukraine);
-                mAdapter = new TeamAdapter(getContext().getApplicationContext(),
-                        R.layout.team_logo_dialog, temp);
-                g.setAdapter(mAdapter);
+
+                if(step==1) {
+                    Drawable img = getContext().getResources().getDrawable(mImage[position]);
+                    img.setBounds( 0, 0, 50, 35 );
+                    teamLogo.setCompoundDrawables( img, null, null, null );
+                    switch (position) {
+                        case 0:
+                            temp = getResources().getStringArray(R.array.football_team_France);
+                            break;
+                        case 1:
+                            temp = getResources().getStringArray(R.array.football_team_Germany);
+                            break;
+                        case 2:
+                            temp = getResources().getStringArray(R.array.football_team_Ukraine);
+                            break;
+                        case 3:
+                            temp = getResources().getStringArray(R.array.football_team_Russia);
+                            break;
+                        case 4:
+                            temp = getResources().getStringArray(R.array.football_team_Spain);
+                            break;
+                        case 5:
+                            temp = getResources().getStringArray(R.array.football_team_Portugal);
+                            break;
+                        case 6:
+                            temp = getResources().getStringArray(R.array.football_team_Turkey);
+                            break;
+                        case 7:
+                            temp = getResources().getStringArray(R.array.football_team_England);
+                            break;
+                        case 8:
+                            temp = getResources().getStringArray(R.array.football_team_Brazil);
+                            break;
+                        case 9:
+                            temp = getResources().getStringArray(R.array.football_team_Argentina);
+                            break;
+                        case 10:
+                            temp = getResources().getStringArray(R.array.football_team_Italy);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    mAdapter = new TeamAdapter(getContext().getApplicationContext(),
+                            R.layout.team_logo_dialog, temp);
+                    g.setAdapter(mAdapter);
+                }
+
+                if(step==2){
+                    step=0;
+                    dismiss();
+                }
             }
         });
 
